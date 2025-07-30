765295995e59535dff10c6ff606ae5f9a5a0ff92
RanitaD27 <ranita27dave@gmail.com>
2025-07-30 13:33:43 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
 	// this serves both the API and the client.
 	// It is the only port that is not firewalled.
 	const port = parseInt(process.env.PORT || '5000', 10);
-	server.listen(port, '127.0.0.1', () => {
+	server.listen(port, () => {
 		log(`Serving on http://127.0.0.1:${port}`);
 	});
 })();
